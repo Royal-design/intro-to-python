969de59928c4d6b8cc8a539263f2cfab691b5910
Royal-design <royaldesign1999@gmail.com>
2026-06-12 15:24:13 +0100

    modules

---
+++ calculator/calc.py
@@ -0,0 +1,8 @@
+def add():
+    return "Addition"
+
+def subtract():
+    return "Subtraction"
+
+def multiply():
+    return "Multiplication"
---
+++ calculator/demo.py
@@ -0,0 +1,14 @@
+
+
+from calc import *
+
+
+result =  add()
+print(result)
+
+result =  subtract()
+print(result)
+
+
+# print(globals())
+# print(__name__)
